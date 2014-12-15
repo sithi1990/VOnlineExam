c33fc07fd906e8e3c92e3bc4802600c9a25e0447
sithi1990 <sithi1990@live.com>
2014-12-15 16:13:52 +0530

    1st Commit

---
+++ ExamEntities/Trainee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamEntities
+{
+    public class Trainee
+    {
+        private int trinee_id;
+
+        public int TraineeID
+        {
+            get { return trinee_id; }
+            set { trinee_id = value; }
+        }
+
+        private string trainee_name;
+
+        public string TraineeName
+        {
+            get { return trainee_name; }
+            set { trainee_name = value; }
+        }
+
+        
+        
+    }
+}
