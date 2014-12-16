652dbb32603daa37693af378b662dbc7c671a1cc
sithi1990 <sithi1990@live.com>
2014-12-16 23:28:38 +0530

    Commit With EF 2

---
+++ DAL-EF/ExamRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DALInterfaces;
+using Entity;
+using System.Data.Entity;
+
+namespace DAL_EF
+{
+    public class ExamRepository : IExamDataAccess
+    {
+        ExaminationContext context = new ExaminationContext();
+        public void AddNew(Examination ex)
+        {
+            context.Examinations.Add(ex);
+            context.SaveChanges();
+        }
+
+        public void Update(Examination ex)
+        {
+            context.Entry(ex).State = EntityState.Modified;
+            context.SaveChanges();
+            
+        }
+
+        public void Delete(int examid)
+        {
+            Examination sub = context.Examinations.Find(examid);
+            context.Examinations.Remove(sub);
+            context.SaveChanges();
+        }
+
+        public IEnumerable<Examination> GetExams()
+        {
+            return context.Examinations.ToList<Examination>();
+        }
+
+
+      
+    }
+}
---
+++ DAL-EF/ExaminationContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace DAL_EF
+{
+    public class ExaminationContext :DbContext
+    {
+        public ExaminationContext()
+            : base("ExaminationContext")
+        {
+        }
+        
+        public DbSet<Choice> Choices { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Examination> Examinations { get; set; }
+        public DbSet<Material> Materials { get; set; }
+
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Trainee> Trainees { get; set; }
+
+        public DbSet<TraineeExamEnrollment> TraineeExamEnrollments { get; set; }
+        public DbSet<TraineeCourseEnrollment> TraineeCourseEnrollments { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
