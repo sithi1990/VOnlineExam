5db1db6fecf165643c6116f707adb69e93f647d2
sithi1990 <sithi1990@live.com>
2014-12-16 23:32:36 +0530

    Commit EF 3

---
+++ Entity/Trainee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Trainee
+    {
+        public int ID { get; set; }
+        public string TraineeName { get; set; }//
+
+        public virtual ICollection<TraineeExamEnrollment> TraineeExamEnrollments { get; set; }
+        public virtual ICollection<TraineeCourseEnrollment> TraineeCourseEnrollments { get; set; }
+    }
+}
